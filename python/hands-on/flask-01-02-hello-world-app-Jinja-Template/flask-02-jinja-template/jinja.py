a11f879d0118f60fe07ae87f179ac03fb324565d
fevzitopcu <fevzitopcu@gmail.com>
2022-01-19 00:06:45 +0300

    flask-01 and flask-02 added

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -9,8 +9,13 @@ def head():
 
 @app.route('/mult')
 def number():
-    var1, var2 = 340, 740
+    var1, var2 = 3430, 3740
     return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+
+@app.route('/divide')
+def number():
+    var1, var2 = 3430, 3740
+    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1/var2)
 
 if __name__ == "__main__":
     app.run(debug=True)
